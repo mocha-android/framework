1ffaac2bdc0d7353d30a5ecd7d29dbc12a234c38
Shaun Harrison <mail@shnhrrsn.com>
2013-01-29 21:16:58 -0500

    Build out NavigationItem, BarButtonItem and BarItem

---
+++ src/mocha/ui/BarButtonItem.java
@@ -0,0 +1,94 @@
+package mocha.ui;
+
+import mocha.graphics.Image;
+
+public class BarButtonItem extends BarItem {
+	enum SystemItem {
+		DONE, CANCEL, EDIT, SAVE, FLEXIBLE_SPACE, FIXED_SPACE,
+		COMPOSE, REPLY, ACTION, ORGANIZE, BOOKMARKS, SEARCH,
+		REFRESH, STOP, CAMERA, TRASH, PLAY, PAUSE, REWIND,
+		FAST_FORWARD, UNDO, REDO
+	}
+
+	enum Style {
+		PLAIN, BORDERED, DONE
+	}
+
+	interface Action {
+		public void action(BarButtonItem barButtonItem);
+	}
+
+	private Style style;
+	private float width;
+	private View customView;
+	private Action action;
+	private boolean isSystemItem;
+	private SystemItem systemItem;
+
+	public BarButtonItem() {
+		this.style = Style.PLAIN;
+	}
+
+	private BarButtonItem(Action action) {
+		this();
+
+		this.action = action;
+	}
+
+	public BarButtonItem(SystemItem systemItem, Action action) {
+		this(action);
+
+		this.isSystemItem = true;
+		this.systemItem = systemItem;
+	}
+
+	public BarButtonItem(View customView) {
+		this();
+		this.customView = customView;
+	}
+
+	public BarButtonItem(String title, Style style, Action action) {
+		this(action);
+		this.setTitle(title);
+	}
+
+	public BarButtonItem(Image image, Style style, Action action) {
+		this(action);
+		this.setImage(image);
+	}
+
+	public Style getStyle() {
+		return style;
+	}
+
+	public void setStyle(Style style) {
+		this.style = style;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public View getCustomView() {
+		return this.isSystemItem ? null : customView;
+	}
+
+	public void setCustomView(View customView) {
+		if(!this.isSystemItem) {
+			this.customView = customView;
+		}
+	}
+
+	public Action getAction() {
+		return action;
+	}
+
+	public void setAction(Action action) {
+		this.action = action;
+	}
+
+}
---
+++ src/mocha/ui/BarItem.java
@@ -0,0 +1,61 @@
+/*
+ *  @author Shaun
+ *	@date 1/29/13
+ *	@copyright	2013 TV Guide, Inc. All rights reserved.
+ */
+package mocha.ui;
+
+import mocha.graphics.Image;
+
+public class BarItem extends mocha.foundation.Object {
+	private Image image;
+	private String title;
+	private boolean enabled;
+	private EdgeInsets imageInsets;
+	private int tag;
+
+	public BarItem() {
+		this.enabled = true;
+		this.imageInsets = EdgeInsets.zero();
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public EdgeInsets getImageInsets() {
+		return imageInsets;
+	}
+
+	public void setImageInsets(EdgeInsets imageInsets) {
+		this.imageInsets = imageInsets;
+	}
+
+	public int getTag() {
+		return tag;
+	}
+
+	public void setTag(int tag) {
+		this.tag = tag;
+	}
+}
