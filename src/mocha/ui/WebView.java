43295e948001bc26f61695f97fe7f369d253e789
Shaun Harrison <mail@shnhrrsn.com>
2014-03-17 18:07:23 -0400

    Drastically improved JS communication in mocha.ui.WebView.  Also fixed some memory leaks in it.

--- src/mocha/ui/WebView.java
+++ src/mocha/ui/WebView.java
@@ -8,16 +8,13 @@ package mocha.ui;
 import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.util.Log;
-import android.webkit.ConsoleMessage;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebChromeClient;
-import android.widget.Toast;
-import android.webkit.JavascriptInterface;
+import android.webkit.*;
 import mocha.graphics.Rect;
 
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 public class WebView extends View {
 
@@ -41,10 +38,12 @@ public class WebView extends View {
 	private NativeView<android.webkit.WebView> nativeView;
 	private Delegate delegate;
 	private boolean isLoading;
-	private boolean ignorePageLoadChanges;
 	private boolean scalesPageToFit;
 	private EvaluateJavascriptInterface evaluateJavascriptInterface;
 	private NavigationType lastNavigationType;
+
+	private static Method evaluateJavascriptMethod = null;
+	private static boolean loadedEvaluateJavascriptMethod = false;
 
 	public WebView(Rect frame) { super(frame); }
 	public WebView() { super(); }
@@ -52,6 +51,16 @@ public class WebView extends View {
 	@Override
 	protected void onCreate(Rect frame) {
 		super.onCreate(frame);
+
+		if(!loadedEvaluateJavascriptMethod) {
+			try {
+				evaluateJavascriptMethod = android.webkit.WebView.class.getMethod("evaluateJavascript", String.class, ValueCallback.class);
+			} catch (NoSuchMethodException e) {
+				evaluateJavascriptMethod = null;
+			}
+
+			loadedEvaluateJavascriptMethod = true;
+		}
 
 		this.setClipsToBounds(true);
 
@@ -70,37 +79,35 @@ public class WebView extends View {
 		this.evaluateJavascriptInterface.register();
 
 		this.setScalesPageToFit(false);
+	}
 
-		this.webView.setWebChromeClient(new WebChromeClient() {
-			private static final String LOG_TAG = "MochaWebView";
+	public void enableJavascriptLogging() {
+		this.webView.setWebChromeClient(WebChromeClient.instance());
+	}
 
-			public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
-				String message = consoleMessage.message() + " -- From line " + consoleMessage.lineNumber() + " of " + consoleMessage.sourceId();
-
-				switch (consoleMessage.messageLevel()) {
-					case DEBUG:
-					case TIP:
-						Log.d(LOG_TAG, message);
-						break;
-					case ERROR:
-						Log.e(LOG_TAG, message);
-						break;
-					case LOG:
-						Log.i(LOG_TAG, message);
-						break;
-					case WARNING:
-						Log.w(LOG_TAG, message);
-						break;
-				}
-				return true;
-			}
-		});
+	public void disableJavascriptLogging() {
+		this.webView.setWebChromeClient(null);
 	}
 
 	@Override
 	public void setUserInteractionEnabled(boolean userInteractionEnabled) {
 		super.setUserInteractionEnabled(userInteractionEnabled);
 		this.nativeView.setUserInteractionEnabled(userInteractionEnabled);
+	}
+
+	public void willMoveToWindow(Window newWindow) {
+		super.willMoveToWindow(newWindow);
+
+		if(newWindow == null) {
+			if(this.nativeView.getSuperview() != null) {
+				this.nativeView.removeFromSuperview();
+			}
+		} else {
+			if(this.nativeView.getSuperview() == null) {
+				this.addSubview(this.nativeView);
+				this.nativeView.setFrame(this.getBounds());
+			}
+		}
 	}
 
 	/**
@@ -201,13 +208,30 @@ public class WebView extends View {
 	}
 
 	/**
-	 * Execute javascript on the current page and return it's response
+	 * Execute javascript on the current page
 	 *
 	 * @param javascript Javascript to evaluate
-	 * @return Javascript response
 	 */
-	public String getStringByEvaluatingJavaScriptFromString(String javascript) {
-		return this.evaluateJavascriptInterface.getStringByEvaluatingJavaScriptFromString(javascript);
+	public void executeJavascript(String javascript) {
+		this.evaluateJavascript(javascript, null);
+	}
+
+	/**
+	 * Execute javascript on the current page and notify a callback of it's response
+	 *
+	 * @param javascript Javascript to evaluate
+	 * @param callback Called on the main thread with the response of the execution
+	 */
+	public void evaluateJavascript(String javascript, ValueCallback <String> callback) {
+		if(evaluateJavascriptMethod != null) {
+			try {
+				evaluateJavascriptMethod.invoke(this.webView, javascript, callback);
+			} catch (Exception e) {
+				MWarn(e, "Javascript execution failed: '%s'", javascript);
+			}
+		} else {
+			this.evaluateJavascriptInterface.evaluateJavascript(javascript, callback);
+		}
 	}
 
 	/**
@@ -321,71 +345,127 @@ public class WebView extends View {
 	 * https://github.com/lolboxen/AndroidNIWebView/blob/master/NIWebView.java
 	 */
 	private class EvaluateJavascriptInterface {
-		private CountDownLatch compileLatch;
-		private CountDownLatch finishLatch;
-		private String returnValue;
-		private String command;
+		private Map<String, Command> commands = new HashMap<String, Command>();
+		private int executionOrdinal = 0;
 		private static final String JS_CODE = "javascript:try { " +
-				"__mochaWebViewJSInterface.setReturnValue(eval(__mochaWebViewJSInterface.getCommand()));" +
-				"} catch(err) { __mochaWebViewJSInterface.setReturnValue(''); }";
+				"__mochaWebViewJSInterface.commandExecuted('%s', eval(__mochaWebViewJSInterface.getCommand('%s')));" +
+				"} catch(err) { __mochaWebViewJSInterface.commandExecuted('%s', ''); }";
+
+
+		private class Command {
+			private final ValueCallback<String> callback;
+			private final String key;
+			private final String javascript;
+
+			private boolean failed;
+			private boolean finished;
+			private Runnable compileTimeoutCallback;
+			private Runnable executionTimeoutCallback;
+
+			Command(String key, String javascript, final ValueCallback<String> callback) {
+				this.key = key;
+				this.javascript = javascript;
+				this.callback = callback;
+			}
+
+			void willStart() {
+				this.compileTimeoutCallback = performOnMainAfterDelay(5000, new Runnable() {
+					public void run() {
+						MWarn("Javascript failed to compile: '%s'", javascript);
+						compileTimeoutCallback = null;
+						commandFailed(Command.this);
+						failed = true;
+					}
+				});
+			}
+
+			String get() {
+				if(!failed) {
+					cancelCallbacks(this.compileTimeoutCallback);
+					this.compileTimeoutCallback = null;
+
+					this.executionTimeoutCallback = performOnMainAfterDelay(30000, new Runnable() {
+						public void run() {
+							MWarn("Timed out waiting for javascript to execute: '%s'", javascript);
+							executionTimeoutCallback = null;
+							commandFailed(Command.this);
+							failed = true;
+						}
+					});
+
+					return this.javascript;
+				} else {
+					return null;
+				}
+			}
+
+			String key() {
+				return this.key;
+			}
+
+			void didFinish(String result) {
+				if(this.executionTimeoutCallback != null) {
+					cancelCallbacks(this.executionTimeoutCallback);
+					this.executionTimeoutCallback = null;
+				}
+
+				if(!finished && !failed) {
+					finished = true;
+
+					if(this.callback != null) {
+						this.callback.onReceiveValue(result);
+					}
+				}
+			}
+		}
 
 		public void register() {
 			webView.addJavascriptInterface(this, "__mochaWebViewJSInterface");
 		}
 
-		public String getStringByEvaluatingJavaScriptFromString(String javascript) {
-			this.command = javascript;
+		public void evaluateJavascript(String javascript, final ValueCallback<String> callback) {
+			String key = String.valueOf(++this.executionOrdinal);
+			Command command = new Command(key, javascript, callback);
+			this.commands.put(key, command);
 
-			this.compileLatch = new CountDownLatch(1);
-			this.finishLatch = new CountDownLatch(1);
-
-			ignorePageLoadChanges = true;
-			webView.loadUrl(JS_CODE);
-			String returnValue = this.getReturnValue();
-			ignorePageLoadChanges = false;
-			return returnValue;
+			command.willStart();
+			webView.loadUrl(String.format(JS_CODE, key, key, key));
 		}
 
-		private String getReturnValue() {
-			this.returnValue = "";
-
-			try {
-				if (!this.compileLatch.await(5, TimeUnit.SECONDS)) {
-					MWarn("Script did not compile");
-					return this.returnValue;
-				}
-			} catch (InterruptedException e) {
-				return this.returnValue;
-			}
-
-			try {
-				finishLatch.await(30, TimeUnit.SECONDS);
-				return this.returnValue;
-			} catch (InterruptedException e) {
-				MWarn("Timed out waiting for JS response");
-				return this.returnValue;
+		@JavascriptInterface
+		public String getCommand(String key) {
+			Command command = this.commands.get(key);
+			if(command != null) {
+				return command.get();
+			} else {
+				return null;
 			}
 		}
 
 		@JavascriptInterface
-		public String getCommand() {
-			String command = this.command;
-			this.compileLatch.countDown();
-			this.command = null;
-			return command;
+		public void commandExecuted(String key, String result) {
+			Command command = this.commands.get(key);
+			if(command != null) {
+				command.didFinish(result);
+				commandEnded(command);
+			}
 		}
 
-		@JavascriptInterface
-		public void setReturnValue(String returnValue) {
-			this.returnValue = returnValue;
-			this.finishLatch.countDown();
+		private void commandFailed(Command command) {
+			command.didFinish("");
+			commandEnded(command);
 		}
+
+		private void commandEnded(Command command) {
+			this.commands.remove(command.key());
+		}
+
 	}
 
 	private class WebViewClient extends android.webkit.WebViewClient {
 
 		public boolean shouldOverrideUrlLoading(android.webkit.WebView view, String url) {
-			if(delegate != null && !ignorePageLoadChanges) {
+			if(delegate != null && (url == null || !url.startsWith("javascript:"))) {
 				NavigationType navigationType;
 
 				if(lastNavigationType != null) {
@@ -423,17 +503,26 @@ public class WebView extends View {
 
 		public void onPageStarted(android.webkit.WebView view, String url, Bitmap favicon) {
 			super.onPageStarted(view, url, favicon);
-			this.loadDidStart();
+
+			if(url == null || !url.startsWith("javascript:")) {
+				this.loadDidStart();
+			}
 		}
 
 		public void onPageFinished(android.webkit.WebView view, String url) {
 			super.onPageFinished(view, url);
-			this.loadDidEnd(false);
+
+			if(url == null || !url.startsWith("javascript:")) {
+				this.loadDidEnd(false);
+			}
 		}
 
 		public void onReceivedError(android.webkit.WebView view, int errorCode, String description, String failingUrl) {
 			super.onReceivedError(view, errorCode, description, failingUrl);
-			this.loadDidEnd(true);
+
+			if(failingUrl == null || !failingUrl.startsWith("javascript:")) {
+				this.loadDidEnd(true);
+			}
 		}
 
 		public void onReceivedSslError(android.webkit.WebView view, SslErrorHandler handler, SslError error) {
@@ -444,7 +533,7 @@ public class WebView extends View {
 		private void loadDidStart() {
 			isLoading = true;
 
-			if(delegate != null && !ignorePageLoadChanges) {
+			if(delegate != null) {
 				delegate.didStartLoad(WebView.this);
 			}
 		}
@@ -452,7 +541,7 @@ public class WebView extends View {
 		public void loadDidEnd(boolean failed) {
 			isLoading = false;
 
-			if(delegate != null && !ignorePageLoadChanges) {
+			if(delegate != null) {
 				if(failed) {
 					delegate.didFailLoad(WebView.this);
 				} else {
@@ -465,5 +554,38 @@ public class WebView extends View {
 
 	}
 
+	private static class WebChromeClient extends android.webkit.WebChromeClient {
+		private static final String LOG_TAG = "MochaWebView";
+		private static WeakReference<WebChromeClient> instance;
+
+		static WebChromeClient instance() {
+			if(instance == null || instance.get() == null) {
+				instance = new WeakReference<WebChromeClient>(new WebChromeClient());
+			}
+
+			return instance.get();
+		}
+
+		public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+			String message = consoleMessage.message() + " -- From line " + consoleMessage.lineNumber() + " of " + consoleMessage.sourceId();
+
+			switch (consoleMessage.messageLevel()) {
+				case DEBUG:
+				case TIP:
+					Log.d(LOG_TAG, message);
+					break;
+				case ERROR:
+					Log.e(LOG_TAG, message);
+					break;
+				case LOG:
+					Log.i(LOG_TAG, message);
+					break;
+				case WARNING:
+					Log.w(LOG_TAG, message);
+					break;
+			}
+			return true;
+		}
+	}
 
 }
