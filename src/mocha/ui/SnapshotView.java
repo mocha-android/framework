1f2eb3a73a97e919031ddb9a0afc3dff131cdb2c
Shaun Harrison <mail@shnhrrsn.com>
2014-05-21 18:33:44 -0400

    Added basic SnapshotView support

--- src/mocha/ui/SnapshotView.java
+++ src/mocha/ui/SnapshotView.java
@@ -5,19 +5,41 @@
  */
 package mocha.ui;
 
+import android.graphics.Bitmap;
+import mocha.graphics.Context;
+import mocha.graphics.Image;
 import mocha.graphics.Rect;
 
 class SnapshotView extends ImageView {
 	private View underlyingView;
 
+
 	public SnapshotView(View view) {
 		super(view.getBounds());
 		this.underlyingView = view;
+		this.setImage(this.getSnapshotImage());
 	}
 
 	public SnapshotView(View view, Rect rect, EdgeInsets capInsets) {
 		super(new Rect(0.0f, 0.0f, rect.size.width, rect.size.height));
 		this.underlyingView = view;
+		throw new IllegalArgumentException("Not yet implemented.");
+	}
+
+	private Image getSnapshotImage() {
+		Rect bounds = this.underlyingView.getBounds();
+
+		try {
+			// TODO: Optimize.
+
+			Context context = new Context(bounds.size, this.underlyingView.scale, Bitmap.Config.ARGB_8888);
+			this.underlyingView.getLayer().renderInContext(context);
+			return context.getImage();
+		} catch (OutOfMemoryError ignored) {
+
+		}
+
+		return null;
 	}
 
 	@Override
