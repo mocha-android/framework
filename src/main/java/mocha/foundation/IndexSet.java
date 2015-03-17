ae72b70f56e6c714d27eb85b5bb965368667cb74
Shaun Harrison <mail@shnhrrsn.com>
2015-03-16 20:42:30 -0400

    Built out IndexSet to include ability to create with Range, get the first/last index and conver to a sort int array

--- src/main/java/mocha/foundation/IndexSet.java
+++ src/main/java/mocha/foundation/IndexSet.java
@@ -5,6 +5,7 @@
  */
 package mocha.foundation;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -22,6 +23,48 @@ public class IndexSet extends HashSet<Integer> {
 
 	public IndexSet(Collection<? extends Integer> collection) {
 		super(collection);
+	}
+
+	public IndexSet(Range range) {
+		super((int)range.length);
+
+		long length = range.max();
+		for(long i = range.location; i < length; i++) {
+			this.add((int)i);
+		}
+	}
+
+	public int getFirstIndex() {
+		int size = this.size();
+
+		if(size > 0) {
+			return this.toIntArray()[0];
+		} else {
+			return -1;
+		}
+	}
+
+	public int getLastIndex() {
+		int size = this.size();
+
+		if(size > 0) {
+			return this.toIntArray()[size - 1];
+		} else {
+			return -1;
+		}
+	}
+
+	public int[] toIntArray() {
+		int[] array = new int[this.size()];
+
+		int idx = 0;
+		for(Integer i : this) {
+			array[idx++] = i;
+		}
+
+		Arrays.sort(array);
+
+		return array;
 	}
 
 }
